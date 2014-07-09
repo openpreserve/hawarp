61e73b539fc68e40367a90a2c36b2e9b1e4f6efc
shsdev <shsschlarb-dev@yahoo.de>
2014-07-09 10:40:07 +0200

    PhantomJS script for taking wayback snapshots added

---
+++ cdx-creator/scripts/storepngsnapshot.js
@@ -0,0 +1,20 @@
+var page = require('webpage').create(),
+  system = require('system'),
+  address, outpng;
+
+if (system.args.length !== 3) {
+  console.log('Usage: phantomjs storepngsnapshot.js <some URL> <png output image path>');
+  phantom.exit();
+}
+
+address = system.args[1];
+outpng = system.args[2];
+page.open(address, function(status) {
+  if (status !== 'success') {
+    console.log('FAIL to load the address');
+  } else {
+	page.render(outpng);
+  }
+  phantom.exit();
+});
+
